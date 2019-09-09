48886b240311f6985409677165aa6b66c2384409
Cuong Nguyen <cuongnp@cardano-labo.com>
2019-09-09 08:44:32 +0700

    fix bug retrieve token

--- test/token.test.js
+++ test/token.test.js
@@ -1,6 +1,7 @@
 const Assert = require('assert');
 const TokenProvider = require('../lib/token');
 const ConfigTest = require('./config.test');
+const Messages = require('../lib/messages');
 
 var tokenProvider = null;
 
@@ -12,6 +13,56 @@ describe('token', function() {
       url: ConfigTest.AUTH_URL,
     });
   });
+
+  describe('constructor', async () => {
+
+    it('Lacking option', async() => {
+      try {
+        tokenProvider = new TokenProvider();
+        Assert.fail('should be error');
+      } catch(err) {
+        console.log('err.message :', err.message);
+        Assert.equal('Missing required parameter options', err.message);
+      }
+    });
+
+    it('Lacking apiKey', async() => {
+      try {
+        tokenProvider = new TokenProvider({
+          secret: ConfigTest.SECRET,
+          url: ConfigTest.AUTH_URL,
+        });
+        Assert.fail('should be error');
+      } catch(err) {
+        Assert.equal('Missing required parameter options.apiKey', err.message);
+      }
+    });
+
+    it('Lacking secret', async() => {
+      try {
+        tokenProvider = new TokenProvider({
+          apiKey: ConfigTest.API_KEY,
+          url: ConfigTest.AUTH_URL,
+        });
+        Assert.fail('should be error');
+      } catch(err) {
+        Assert.equal('Missing required parameter options.secret', err.message);
+      }
+    });
+
+    it('Lacking url', async() => {
+      try {
+        tokenProvider = new TokenProvider({
+          apiKey: ConfigTest.API_KEY,
+          secret: ConfigTest.SECRET,
+        });
+        Assert.fail('should be error');
+      } catch(err) {
+        Assert.equal('Missing required parameter options.url', err.message);
+      }
+    })
+
+  })
 
   describe('#getLatestToken()', async () => {
     it('Should get new token', async () => {
@@ -31,6 +82,17 @@ describe('token', function() {
       let token2 = await tokenProvider.getLatestToken();
       Assert.equal(token1, token2, 'Token must be same');
     });
+
+    it('Clear token', async () => {
+      let oldToken = 'This is old token';
+      tokenProvider.setToken(oldToken);
+      let token = await tokenProvider.getLatestToken();
+      Assert.equal(oldToken, token, 'Token must be same');
+      tokenProvider.clearToken();
+      token = await tokenProvider.getLatestToken();
+      Assert.notEqual(oldToken, token, 'Token must be same');
+    });
+
   });
 
   describe('#getNewToken()', async () => {
@@ -39,24 +101,50 @@ describe('token', function() {
 
       Assert.ok(token.length > 0, 'must have token');
     });
+
+    it('invalid key', async () => {
+      tokenProvider = new TokenProvider({
+        apiKey: ConfigTest.API_KEY,
+        secret: ConfigTest.SECRET + '_invalid',
+        url: ConfigTest.AUTH_URL,
+      });
+      try {
+        await tokenProvider.getNewToken();
+        Assert.fail('should be error');
+      } catch (err) {
+        Assert.ok(err.message, Messages.internal_error.message);
+        Assert.ok(err.code, Messages.internal_error.code);
+        Assert.ok(err.httpCode, 400);
+        Assert.ok(err.inner.message, 'Request failed with status code 400');
+        Assert.ok(err.inner.response.data.cd, 400);
+        Assert.ok(err.inner.response.data.message, 'Bad Request');
+      }
+    });
   });
 
-  describe('#getNewTokenOnce)', async () => {
+  describe('#getNewTokenOnce()', async () => {
+    it('Get new token 1 time', async () => {
+      let token = await tokenProvider.getNewTokenOnce();
+      Assert.ok(typeof(token), 'string');
+      Assert.ok(token.length > 0);
+    });
+
     it('Get new token', async () => {
       let firstToken = null;
       var getTokenAsync = async i => {
         let token = await tokenProvider.getNewTokenOnce();
+        Assert.ok(typeof(token), 'string');
+        Assert.ok(token.length > 0);
         if (firstToken == null) {
           firstToken = token;
         } else {
-          Assert.ok(firstToken, token, 'Token must be the same');
+          Assert.ok(firstToken, token, `Token must be the same (${i})`);
         }
-        console.log('token :', i, token);
+        // console.log('token :', i, token);
       };
 
-      for (let i = 0; i < 50; i++) {
-        getTokenAsync(i);
-      }
+      let list = [...Array(100).keys()];
+      return await Promise.all(list.map(item => getTokenAsync(item)));
     });
   });
 
@@ -73,7 +161,6 @@ describe('token', function() {
         xTime: 1547524471,
       };
       let checksum = await tokenProvider.getChecksum(opt);
-      console.log('checksum :', checksum);
       Assert.equal(
         checksum,
         '8b575ddba41f8fb65536b27b71cefd477b69fb3d02ba290902c5761f3d2e6e65'
